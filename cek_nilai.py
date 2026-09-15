9b8159ac68e5accfe0229392119a5962fe85ab28
Hussein <lonelycompany99@gmail.comm>
2026-09-15 20:24:41 +0700

    Sesi5

---
+++ cek_nilai.py
@@ -0,0 +1,7 @@
+nilai = int(input("Berapa nilai ujianmu? "))
+
+# Lengkapi if else di bawah ini (ingat tool)
+if nilai >= 70:
+    print("Selamat! kamu lulus ujian.")
+else:
+    print("Maaf, kamu tidak lulus ujian. Coba lagi yo!")
